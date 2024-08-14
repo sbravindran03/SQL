8c1221b6a6ac85253ce72f2321b2eb7fdcce6021
sbravindran03 <ravindran5211@gmail.com>
2024-08-14 16:48:10 +0530

    commit

---
+++ q19.sql
@@ -0,0 +1,3 @@
+-- write the query display NAME AND desgination  along WITH penalty  OF 100 in salary
+select ename ,job , (sal-100) as penalty
+from emp;
---
+++ q20.sql
@@ -0,0 +1,3 @@
+-- To display the name and salary of employees with a monthly hike of 50 added to their salary, you can use the following SQL query:
+select ename , (sal+50) as hikemonthly
+from emp;
